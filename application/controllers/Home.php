e3476bb300c7c36977cce561c8525fb1bb1fe192
mugishap <precieuxmugisha@gmail.com>
2022-06-17 22:26:04 +0200

    finishing up landing page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,8 @@
 class Home extends CI_Controller{
     public function __construct(){
         parent::__construct();
+    }
+    public function index(){
         $this->load->view('html/users/home');
     }
 }
